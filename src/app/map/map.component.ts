f42ec01c07031fa8ea26ea540453d1c6667bc435
nmd974 <jha.payet@gmail.com>
2022-09-24 22:04:55 +0400

    affichage de la map

---
+++ src/app/map/map.component.ts
@@ -0,0 +1,31 @@
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import * as L from 'leaflet';
+
+@Component({
+  selector: 'app-map',
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.css']
+})
+export class MapComponent implements AfterViewInit {
+  private map: any;
+
+  private initMap(): void {
+    this.map = L.map('map', {
+      center: [ -21.114063, 55.4998053 ],
+      zoom: 11
+    });
+    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 18,
+      minZoom: 3,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    });
+
+    tiles.addTo(this.map);
+  }
+  constructor() { }
+
+  ngAfterViewInit(): void {
+    this.initMap();
+  }
+
+}
